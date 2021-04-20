d9cb6a92d1b11aab522d032d55756c3fb4739d8a
Leny-L <65807394+Leny-L@users.noreply.github.com>
2021-04-20 14:44:42 +0300

    test parsing:  - small fixes

--- src/main/java/com/BestBot/Core/Parsers/TestParser.java
+++ src/main/java/com/BestBot/Core/Parsers/TestParser.java
@@ -1,21 +1,14 @@
 package com.BestBot.Core.Parsers;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Service
 @Component
 public class TestParser implements CrossoutdbParser{
 
-    private String api;
-
-    private LocalDateTime timeLastApiResponse; //возраст курса доллара
+    private final String api;
 
     public TestParser(@Value("${api}") String api) {
         this.api = api;

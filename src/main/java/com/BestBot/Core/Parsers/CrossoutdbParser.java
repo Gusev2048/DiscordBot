a722e4fea00ab45fa8775778c76e6ff8c408b4c0
Leny-L <65807394+Leny-L@users.noreply.github.com>
2021-04-22 14:59:08 +0300

    small fixes

--- src/main/java/com/BestBot/Core/Parsers/CrossoutdbParser.java
+++ src/main/java/com/BestBot/Core/Parsers/CrossoutdbParser.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 public interface CrossoutdbParser {
     String getApi();
-    List<Object> testTest();
+//    List<Object> testTest();
 
 }
--- src/main/java/com/BestBot/Core/Parsers/TestParser.java
+++ src/main/java/com/BestBot/Core/Parsers/TestParser.java
@@ -5,10 +5,10 @@ import com.BestBot.Core.Entity.ItemEntity;
 import com.BestBot.Core.Repository.ItemEntityRepository;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import net.dv8tion.jda.api.JDA;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.scheduling.annotation.Scheduled;
 import reactor.core.publisher.Flux;
 
@@ -16,13 +16,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-@Service
 @Component
 public class TestParser implements CrossoutdbParser{
 
@@ -32,10 +31,24 @@ public class TestParser implements CrossoutdbParser{
 
     private ItemEntityRepository itemEntityRepository;
 
-    public TestParser(@Value("${getItemApi}") String api, ItemEntityRepository itemEntityRepositoryNew, MessageSender messageSender) {
+    private JDA jda;
+
+    private List<ItemEntity> itemList;
+
+    public TestParser(@Value("${getItemApi}") String api, ItemEntityRepository itemEntityRepositoryNew, MessageSender messageSender, JDA jda) {
         this.api = api;
         this.itemEntityRepository = itemEntityRepositoryNew;
         this.messageSender = messageSender;
+        this.jda = jda;
+        this.itemList = new ArrayList<>();
+    }
+
+    @Bean
+    @Scheduled(fixedDelay = 300000)
+    void saveEtities(){
+        testTest().stream()
+                .filter(e -> e instanceof ItemEntity)
+                .forEach(e -> itemEntityRepository.save((ItemEntity) e));
     }
 
     @Override
@@ -54,51 +67,39 @@ public class TestParser implements CrossoutdbParser{
         return new String(chars, StandardCharsets.UTF_8);
     }
 
-    @Override
     public List<Object> testTest(){
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNodeAll;
 
         List<Object> items = new ArrayList<>();
 
+        messageSender.sengMessage(jda, ("db renew START " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH.mm.ss.AAAA"))));
         try{
             jsonNodeAll = objectMapper.readTree(readFromApi());
             Flux.fromIterable(jsonNodeAll)
                     .map(jsonNode -> {
                                 ItemEntity e = new ItemEntity();
-                                        e.setId(jsonNode.get("id").asLong());
-                                        e.setName(jsonNode.get("name").asText());
-                                        e.setCategory(jsonNode.get("categoryName").asText());
-                                        e.setSellPrice(jsonNode.get("sellPrice").asInt());
-                                        e.setBuyPrice(jsonNode.get("buyPrice").asInt());
-                                        e.setSellOffers(jsonNode.get("sellOffers").asInt());
-                                        e.setBuyOrders(jsonNode.get("buyOrders").asInt());
-                                        return e;
+                                e.setId(jsonNode.get("id").asLong());
+                                e.setName(jsonNode.get("name").asText());
+                                e.setCategory(jsonNode.get("categoryName").asText());
+                                e.setSellPrice(jsonNode.get("sellPrice").asInt());
+                                e.setBuyPrice(jsonNode.get("buyPrice").asInt());
+                                e.setSellOffers(jsonNode.get("sellOffers").asInt());
+                                e.setBuyOrders(jsonNode.get("buyOrders").asInt());
+                                return e;
                             }
                     )
                     .subscribe(items::add);
         }catch (Exception e){
-            e.printStackTrace();
+            messageSender.sengMessage(jda, ("db renew ERROR " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH.mm.ss.AAAA"))));
         }
+        messageSender.sengMessage(jda, ("db renew COMPLETE " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH.mm.ss.AAAA"))));
+
+        this.itemList = itemList.stream()
+                .filter(Objects::nonNull)
+                .toList();
+
+        this.itemList.forEach(e-> System.out.println(e.getClass()));
         return items;
-    }
-
-    @Bean
-    @Scheduled(fixedDelay = 300000)
-    void saveEtities(){
-//        return args-> {
-            testTest().stream()
-                    .filter(e -> e instanceof ItemEntity)
-                    .forEach(e -> itemEntityRepository.save((ItemEntity) e));
-//            messageSender.sengMessage("Finish db update at " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH.mm.ss.AAAA")));
-                    System.out.println("Ololo " + itemEntityRepository.toString());
-//        };
-    }
-
-    @Bean
-    @Scheduled(fixedDelay = 100000)
-    void ololo() {
-//            messageSender.sengMessage("sendOlolo");
-//            System.out.println("Ololo " + itemEntityRepository.toString());
     }
 }
